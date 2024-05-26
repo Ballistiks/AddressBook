8345d94b6459f5d535afae71fdf5e05d02c2384d
Ballistiks <daragao689@gmail.com>
2024-05-26 19:07:46 -0300

    FluentValidation

--- Models/UsuarioModel.cs
+++ Models/UsuarioModel.cs
@@ -8,16 +8,13 @@ namespace AddressBook.Models
 		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
 		public int Id { get; set; }
 
-		[Required]
 		[MaxLength(50)]
 		public string? Nome { get; set; }
 
-		[Required]
 		[MaxLength(255)]
 		public string? Login { get; set; }
 
-		[Required]
-		[MaxLength(80)]
+		[MaxLength(255)]
 		public string? Senha { get; set; }
 	}
 }
