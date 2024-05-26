5b323c9df3b89eccffc02adf4792b7d0e97b024b
Ballistiks <daragao689@gmail.com>
2024-05-26 16:30:52 -0300

    Criado Controller e Model

---
+++ Controllers/EnderecoController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AddressBook.Controllers
+{
+    public class EnderecoController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Novo ()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult Deletar()
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ Models/EnderecoModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace AddressBook.Models
+{
+	public class EnderecoModel
+	{
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Id { get; set; }
+
+		[MaxLength(80)]
+		public string? Logradouro { get; set; }
+
+		[MaxLength(80)]
+		public string? Complemento { get; set; }
+
+		[MaxLength(80)]
+		public string? Bairro { get; set; }
+
+		[MaxLength(80)]
+		public string? Cidade { get; set; }
+
+		[MaxLength(3)]
+		public string? UF { get; set; }
+
+		[MaxLength(10)]
+		public string? Cep { get; set; }
+
+	}
+}
