bc310d253f466cf1e6638de9dabd3741f015aa74
Ballistiks <daragao689@gmail.com>
2024-05-26 16:36:59 -0300

    View Novo e Controller EnderecoController

--- Controllers/EnderecoController.cs
+++ Controllers/EnderecoController.cs
@@ -3,30 +3,51 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AddressBook.Controllers
 {
-    public class EnderecoController : Controller
-    {
+	public class EnderecoController : Controller
+	{
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+		public IActionResult Index()
+		{
+			return View();
+		}
 
-        public IActionResult Novo ()
-        {
-            return View();
-        }
+		public IActionResult Novo()
+		{
+			return View();
+		}
 
-        public IActionResult Editar()
-        {
-            return View();
-        }
+		public IActionResult Editar()
+		{
+			return View();
+		}
 
-        public IActionResult Deletar()
-        {
-            return View();
-        }
+		public IActionResult Deletar()
+		{
+			return View();
+		}
 
 
 
-    }
+		public FileContentResult Exportar()
+		{
+			string csv = "\"Usuario_Id\",\"Logradoura\",\"Bairro\",\"Complemento\",\"Cidade\",\"UF\" \n";
+			var enderecos = ""
+			foreach (var lista in enderecos)
+			{
+				csv = csv + string.Format("\"{0}\",\"{1}\",\"{2}\",\"{3}\",\"{4}\",\"{5}\" \n",
+										   lista.Usuario_Id,
+											lista.Logradouro,
+										  lista.Bairro,
+										   lista.Complemento,
+										   lista.Cidade,
+										   lista.UF);
+			}
+			//StringWriter sw = new StringWriter();
+			//sw.WriteLine("\"ID\",\"Title\",\"Release Date\",\"Genere\",\"Price\",\"Rating\"");
+			return File(new System.Text.UTF8Encoding().GetBytes(csv), "text/csv", "Enderecos.csv");
+		}
+
+
+
+	}
 }
---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,10 @@
+﻿using AddressBook.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AddressBook.Data
+{
+	public class DatabaseContext(DbContextOptions<DatabaseContext> options) : DbContext(options)
+	{
+		public DbSet<EnderecoModel> Enderecos { get; set; }
+	}
+}
