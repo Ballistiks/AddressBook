1c6fc21a9c7e01b2bd66a05ad96d4f8b243e74be
Ballistiks <daragao689@gmail.com>
2024-05-26 16:57:40 -0300

    Migrations

---
+++ Migrations/20240526195536_Enderecos.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AddressBook.Migrations
+{
+    /// <inheritdoc />
+    public partial class Enderecos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Enderecos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Logradouro = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: true),
+                    Complemento = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: true),
+                    Bairro = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: true),
+                    Cidade = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: true),
+                    UF = table.Column<string>(type: "nvarchar(3)", maxLength: 3, nullable: true),
+                    Cep = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Enderecos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Enderecos");
+        }
+    }
+}
---
+++ Models/UsuarioModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace AddressBook.Models
+{
+	public class UsuarioModel
+	{
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Id { get; set; }
+
+		[MaxLength(50)]
+		public string? Nome { get; set; }
+
+		[MaxLength(255)]
+		public string? Login { get; set; }
+
+		[MaxLength(80)]
+		public string? Senha { get; set; }
+	}
+}
