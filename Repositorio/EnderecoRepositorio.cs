f8e1c69226a0c5c9f921257ba91c8ff292c7d446
Ballistiks <daragao689@gmail.com>
2024-05-26 17:11:37 -0300

    Nothing

--- Repositorio/EnderecoRepositorio.cs
+++ Repositorio/EnderecoRepositorio.cs
@@ -1,12 +1,20 @@
-﻿using AddressBook.Models;
+﻿using AddressBook.Data;
+using AddressBook.Models;
+using Microsoft.Identity.Client;
 
 namespace AddressBook.Repositorio
 {
+	private readonly DatabaseContext databaseContext;
 	public class EnderecoRepositorio : IEnderecoRepositorio
 	{
+		public EnderecoRepositorio(DatabaseContext databaseContext)
+		{
+			databaseContext = DatabaseContext;
+		}
+
 		public EnderecoModel NovoEndereco(EnderecoModel endereco)
 		{
-			throw new NotImplementedException();
+
 		}
 	}
 }
